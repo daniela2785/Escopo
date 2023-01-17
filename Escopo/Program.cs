b2a26a9b13fda798146c73454cd051a258f3a8b7
daniela marques <dd978452@gmail.com>
2023-01-17 12:19:02 -0300

    Adicionar arquivos de projeto.

---
+++ Escopo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Escopo {
+    class Program
+    {
+        //declarando variavel no escopo global para que ela possa ser utilizada em qualquer função
+        static string meuNomeCompleto = "Daniela Marques";
+
+       //primeira função
+       static void Main(string[] args)
+        {
+            //string meuNome = "Daniela";
+            Console.WriteLine(meuNomeCompleto);
+            Console.ReadLine();
+        }
+
+        //segunda função
+         static void nome()
+        {
+            Console.WriteLine(meuNomeCompleto);
+            Console.ReadLine();
+        }
+    }
+}
